0b45942a66d66a7d2310504c79554b723482a04c
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-22 20:15:06 +0530

    First Commit

---
+++ src/com/amritesh/hibernate/demo/DeleteCoursesAndReviewsDemo.java
@@ -0,0 +1,40 @@
+package com.amritesh.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.amritesh.hibernate.entity.demo.Course;
+import com.amritesh.hibernate.entity.demo.Instructor;
+import com.amritesh.hibernate.entity.demo.InstructorDetail;
+import com.amritesh.hibernate.entity.demo.Review;
+
+public class DeleteCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
+										.addAnnotatedClass(Course.class)
+										.addAnnotatedClass(Review.class)
+										.buildSessionFactory();
+		Session session = null;
+		try {
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			Course course = session.get(Course.class, 10);
+			System.out.println(course.getReviews());
+			session.delete(course);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+		
+	}
+	
+}
---
+++ src/com/amritesh/hibernate/demo/GetCoursesAndReviewsDemo.java
@@ -0,0 +1,45 @@
+package com.amritesh.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.amritesh.hibernate.entity.demo.Course;
+import com.amritesh.hibernate.entity.demo.Instructor;
+import com.amritesh.hibernate.entity.demo.InstructorDetail;
+import com.amritesh.hibernate.entity.demo.Review;
+
+public class GetCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
+										.addAnnotatedClass(Course.class)
+										.addAnnotatedClass(Review.class)
+										.buildSessionFactory();
+		Session session = null;
+		try {
+			session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+						
+			Course course = new Course("Packman");
+			course.addReview(new Review("Review 1"));
+			course.addReview(new Review("Review 2"));
+			course.addReview(new Review("Review 3"));
+			System.out.println(course);
+			System.out.println(course.getReviews());
+			session.save(course);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+		
+	}
+	
+}
